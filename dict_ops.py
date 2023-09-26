6fef782c85b3d5c9bac10c792d467796e4dfe35e
Vijay-Vaddi <vijayzvaddi@gmail.com>
2023-09-26 18:44:30 +0100

    list and dict ops

---
+++ dict_ops.py
@@ -0,0 +1,19 @@
+new_dict = dict()
+new_dict_list = [
+    {
+    'a':[1,2,3],
+    'b':'Hello',
+    3:123
+    },
+    {
+    'a':[4,5,6],
+    'b':'Hello',
+    3:123
+    },
+    {
+    'a':[1,2,3],
+    'b':'Hello',
+    3:123
+    } 
+]
+print(new_dict_list[1]['a'][2])
