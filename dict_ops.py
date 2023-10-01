1e712013b8b73ff59ad5c66eaab83bbd245781c4
Vijay-Vaddi <vijayzvaddi@gmail.com>
2023-10-01 10:45:43 +0100

    some changes

--- dict_ops.py
+++ dict_ops.py
@@ -13,7 +13,8 @@ new_dict_list = [
     {
     'a':[1,2,3],
     'b':'Hello',
-    3:123
+    'b': '123x' #over writes previous key:value
     } 
 ]
 print(new_dict_list[1]['a'][2])
+print(new_dict_list[2]['b'])
