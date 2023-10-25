84fbc457febda938c65e19818bfe5307f60f2d43
Vijay-Vaddi <vijayzvaddi@gmail.com>
2023-10-25 11:33:00 +0100

    super class

--- OOP.py
+++ OOP.py
@@ -1,12 +1,18 @@
 # Inheritence 
 
 class User:
+    def __init__(self,email):
+        self.email = email
+
     def sign_in(self):
         print('Logged in')
 
 
 class Wizard(User):
-    def __init__(self, name, power):
+    
+    def __init__(self, name, power, email):
+        # User.__init__(self, email)
+        super().__init__(email)
         self.name = name
         self.power = power
     
@@ -22,7 +28,8 @@ class Archers(User):
     def attack(self):
         print(f'Attacking with {self.num_arrows} killer arrows !!')
 
-wizard1 = Wizard('Merllin', 50)
-archer1 = Archers('legolas', 1)
-wizard1.attack()
-archer1.sign_in()
+wizard1 = Wizard('Merllin', 50, '@email')
+print(wizard1.email)
+
+print(isinstance(wizard1, Wizard))
+
