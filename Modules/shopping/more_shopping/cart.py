630e1b2199e56067f6dc8310808d46fe44f7e8b2
Vijay-Vaddi <vijayzvaddi@gmail.com>
2023-11-19 14:45:08 +0000

    Modules

---
+++ Modules/shopping/more_shopping/cart.py
@@ -0,0 +1,5 @@
+print(__name__)
+def add_to_cart(item):
+    cart = []
+    cart.append(item)
+    return cart
---
+++ Modules/utility.py
@@ -0,0 +1,11 @@
+print(__name__)
+
+def sum(num1,num2):
+    return num1+num2
+
+def multiply(num1,num2):
+    return num2*num1
+
+# doesn't run from here
+if __name__ == "__main__":
+    print("Hey its the main file only")
