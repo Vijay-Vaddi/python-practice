625e55a555028d3e1d0f4706a5943a7613157a27
Vijay-Vaddi <vijayzvaddi@gmail.com>
2023-12-09 21:14:54 +0000

    hashing sha1

--- Password_with_python/checkpassword.py
+++ Password_with_python/checkpassword.py
@@ -1,14 +1,25 @@
 import requests
+import hashlib 
 
 #send first 5 chars of hashed password123 as request to api to compare with database
-url = 'https://api.pwnedpasswords.com/range/' + 'cbfda'
-
-res = requests.get(url)
-print(res) 
 
 def request_api_data(query_data):
-    pass
+    url = 'https://api.pwnedpasswords.com/range/' + query_data
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error Fetching: {res.status_code}, check API please')
+    return res
+
 
 def pawned_api_check(password):
-    pass
+    
+    #encode password into utf-8 string, 
+    # use sha1 to hash it, gives sha1 object
+    #digest to return hex value. 
+    # turn to upper case to match with API
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    print(sha1password)
 
+    print(request_api_data(sha1password[:5]))
+
+pawned_api_check('123')
