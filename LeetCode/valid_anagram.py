395454c7de47f39153003dd609b12785fe32e74d
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-01-25 18:51:01 +0000

    valid anagrams

--- LeetCode/valid_anagram.py
+++ LeetCode/valid_anagram.py
@@ -4,3 +4,26 @@
 # Input: s = "anagram", t = "nagaram"
 # Output: true
 
+s = "anagram" 
+t = "nagaram"
+
+dic_s = {}
+dic_t = {}
+
+def check_anagrams(dic_s, dic_t):
+    for char in sorted(s): 
+        if char not in dic_s.keys():
+            dic_s[char] = 1
+        else:
+            dic_s[char]+=1
+
+    for char in sorted(t): 
+        if char not in dic_t.keys():
+            dic_t[char] = 1
+        else:
+            dic_t[char]+=1
+
+    print(dic_t.items(), '\n', dic_s.items())
+    return dic_s.items() == dic_t.items() #dic_t.keys() == dic_s.keys() and dic_s.values() == dic_t.values()
+
+print(check_anagrams(dic_s, dic_t))
