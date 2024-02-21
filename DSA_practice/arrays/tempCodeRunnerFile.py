86637d6f6675ae4f7253a936b014510aba53808a
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-02-21 16:11:17 +0000

    remove duplicates 2

--- DSA_practice/arrays/tempCodeRunnerFile.py
+++ DSA_practice/arrays/tempCodeRunnerFile.py
@@ -1,3 +1,11 @@
+if item == nums[i]:
+        count=count+1
 
-# print(items)
-# print(items.pop())
+    if count>2:
+        nums.remove(item)
+        nums.append('#')
+        k=k+1
+
+    if item!=nums[i]:
+        item = nums[i]
+     
