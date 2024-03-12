de24e8cf7925204e49104b8c370468775f2af137
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-03-12 11:55:03 +0000

    cleaned uo

--- DSA_practice/hash_maps/word_pattern.py
+++ DSA_practice/hash_maps/word_pattern.py
@@ -1,11 +1,5 @@
-'''290. Word Pattern Given a pattern and a string s, find if s follows the same pattern.
-Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
-Input: pattern = "abba", s = "dog cat cat dog"
-Output: true
-Example 2:
-Input: pattern = "abba", s = "dog cat cat fish"
-Output: false'''
-
+'''290. Input: pattern = "abba", s = "dog cat cat dog" , Output: true
+Input: pattern = "abba", s = "dog cat cat fish", Output: false'''
 #each letter in pattern has to match with only one word in sentence. 
 #len of pattern and word count in s will be same. 
 
